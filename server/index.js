3c35fb6391c4b034abe3efcb4073cd6bbdc6c183
Brianna Wang <74729817+b-ultramarine@users.noreply.github.com>
2022-09-25 22:49:46 -0400

    heroku deploy

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const request = require('request');
 const dotenv = require('dotenv');
 
-const port = 3001
+const port = process.env.PORT || 3001;
 
 global.access_token = ''
 
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,9 +1,10 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
+    const port = process.env.PORT || 3001;
     app.use('/auth/**', 
         createProxyMiddleware({ 
-            target: process.env.SERVER
+            target:  'http://localhost:' + port
         })
     );
 };
